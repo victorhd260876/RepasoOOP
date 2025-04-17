2e128ad523fb7c53f754fa983fcfe277c070dd2d
Victor H <victor.huamandiaz@gmail.com>
2025-04-16 19:05:12 -0500

    Creando clase Documentos

---
+++ Documentos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoOOP
+{
+    public class Documentos
+    {
+        public string tipo;
+        public string numero;
+        public DateOnly fechaemis;
+        public DateOnly fechavenc;
+
+        public Documentos(string tipo, string numero, DateOnly fechaemis, DateOnly fechavenc)
+        {
+            this.tipo = tipo;
+            this.numero = numero;
+            this.fechaemis = fechaemis;
+            this.fechavenc = fechavenc;
+        }
+    }
+}
